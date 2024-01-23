23e8fa36566e282687a85f384b943c8165f1bcd8
김윤지 <kkamisister1207@gmail.com>
2024-01-23 14:31:23 +0900

    solved 10811

---
+++ BOJ/240123/B10811.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+bucket_list = [i for i in range(1, N+1)]
+
+for M in range(1, M+1):
+
+    i, j = map(int, input().split())
+
+    temp = []
+
+    temp = bucket_list[i-1:j]
+    temp.reverse()
+
+    bucket_list[i-1:j] = temp
+
+print(bucket_list)
