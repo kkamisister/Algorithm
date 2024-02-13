ce7ac0ca4dc8bf4a1d38e84a0bc9bddbecb37031
김윤지 <kkamisister1207@gmail.com>
2024-02-13 17:54:33 +0900

    -240213

--- BOJ/B2231.py
+++ BOJ/B2231.py
@@ -1,2 +1,15 @@
 N = int(input())
 
+for i in range(1, N):
+    num_list = []
+    a = i
+    for j in range(6):
+        num_list.append(a%10)
+        a //= 10
+    result_list = []
+    if i + sum(num_list) == N:
+        result_list.append(i)
+if len(result_list) != 0:
+    print(result_list[0])
+else:
+    print(0)
