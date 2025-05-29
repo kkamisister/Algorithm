09a0b832e738dede34cc6f3c9a6513aa3e214a71
김윤지 <156289703+kkamisister@users.noreply.github.com>
2025-05-29 17:01:33 +0900

    [level 3] Title: 단어 변환, Time: 0.21 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,32 +1,36 @@
 from collections import deque
 
 def solution(begin, target, words):
+    n = len(words)
     
-    # 사전처리
     if target not in words:
         return 0
-
     
-    visited = set()
+    visited = [0]*n
+    
     queue = deque([(begin, 0)])
-
+    
     while queue:
         current, depth = queue.popleft()
         if current == target:
             return depth
-
-        for word in words:
-            if word in visited:
+        
+        for i in range(n):
+            if visited[i]:
                 continue
+                
+            word = words[i]
 
-            diff = 0
-            for i in range(len(current)):
-                if current[i] != word[i]:
-                    diff += 1
-                    if diff > 1:
+            cnt = 0
+            for j in range(len(word)):
+                if word[j] != current[j]:
+                    cnt += 1
+                    if cnt > 1:
                         break
-
-            if diff == 1:
-                visited.add(word)
+                    
+            if cnt == 1:
+                visited[i] = 1
                 queue.append((word, depth + 1))
-
+    
+    
+    return 0
