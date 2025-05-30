6ef5d9d189c61b5f8fc9f12ffd1c2551fea79cf3
김윤지 <156289703+kkamisister@users.noreply.github.com>
2025-05-30 10:45:24 +0900

    [level 3] Title: 양과 늑대, Time: 20.66 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,34 +1,28 @@
 from collections import defaultdict
-
 def solution(info, edges):
     tree = defaultdict(list)
     for parent, child in edges:
         tree[parent].append(child)
-
+        
     max_sheep = 0
-
     def dfs(current, sheep, wolf, visited):
         nonlocal max_sheep
         if info[current] == 0:
             sheep += 1
         else:
             wolf += 1
-
         if wolf >= sheep:
             return
-
         max_sheep = max(max_sheep, sheep)
-
         for i in range(len(visited)):
             if visited[i]:
                 for child in tree[i]:
                     if not visited[child]:
                         visited_copy = visited[:]
-                        visited_copy[child] = True
+                        visited_copy[child] = 1
                         dfs(child, sheep, wolf, visited_copy)
-
-    visited = [False] * len(info)
-    visited[0] = True
+        
+    visited = [0]*len(info)
+    visited[0] = 1
     dfs(0, 0, 0, visited)
-
     return max_sheep
